03ccb8c38913818b6b806a90206c1397b91ed6f8
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-08 20:10:46 -0300

    file base

---
+++ src/main/java/com/desafioSpring/springValores/SpringValoresApplication.java
@@ -0,0 +1,13 @@
+package com.desafioSpring.springValores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringValoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafioSpring/springValores/SpringValoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioSpring.springValores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

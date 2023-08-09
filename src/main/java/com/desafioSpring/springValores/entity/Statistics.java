952617f673c39816ce5fbbfca9121fb2ec5d3fe3
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-08 22:20:23 -0300

    Statistics.java entity done.

---
+++ src/main/java/com/desafioSpring/springValores/entity/Statistics.java
@@ -0,0 +1,27 @@
+package com.desafioSpring.springValores.entity;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@ToString
+public class Statistics {
+
+    private List<Double> valores;
+
+    private double media;
+
+    private double desvio_padrao;
+
+    private int qtde;
+
+    private double mediano;
+
+}

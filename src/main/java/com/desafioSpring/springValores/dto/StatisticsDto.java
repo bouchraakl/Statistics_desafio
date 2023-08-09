6568146acbabf701b2927d98930981b95ce82246
Bouchra Akl <bushraakl1234@gmail.com>
2023-08-08 22:20:53 -0300

    StatisticsDto.java implemented for better response.

---
+++ src/main/java/com/desafioSpring/springValores/dto/StatisticsDto.java
@@ -0,0 +1,14 @@
+package com.desafioSpring.springValores.dto;
+
+import lombok.Data;
+
+@Data
+public class StatisticsDto {
+    private double media;
+
+    private double desvio_padrao;
+
+    private int qtde;
+
+    private double mediano;
+}
